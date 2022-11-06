b6c6942ab250c94c42fedb35fbfd379a6c0105bc
haloglu <haloglusertac@gmail.com>
2022-11-06 15:44:36 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,6 +120,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -128,5 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
